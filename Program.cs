8a588550e33d24b4815844cccf6af7ccb7bf8756
Jason Palma <jasonpalma16@gmail.com>
2024-09-25 20:34:05 -0400

    Define enum Rank

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 ﻿using System;
 
 namespace _9_25_hw3_decks_and_cards {
+    public enum Rank {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello World!");
