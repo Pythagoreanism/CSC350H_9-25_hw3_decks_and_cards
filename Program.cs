79c57169ef4f635869bb16dd0f8c4a5af687b86b
Jason Palma <jasonpalma16@gmail.com>
2024-09-25 20:51:27 -0400

    Define class Card

--- Program.cs
+++ Program.cs
@@ -26,6 +26,17 @@ namespace _9_25_hw3_decks_and_cards {
     }
 
 
+    public class Card {
+        private Rank rank;
+        private Suit suit;
+
+        // Properties
+        public Rank Rank { get { return rank; } }
+        public Suit Suit { get { return suit; } }
+        
+    };
+
+
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello World!");
