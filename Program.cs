61c8c6dc35208ea2a4349559ee918c7c8442a2d6
Jason Palma <jasonpalma16@gmail.com>
2024-09-25 20:38:47 -0400

    Define enum Suit

--- Program.cs
+++ Program.cs
@@ -18,6 +18,14 @@ namespace _9_25_hw3_decks_and_cards {
     }
 
 
+    public enum Suit {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello World!");
